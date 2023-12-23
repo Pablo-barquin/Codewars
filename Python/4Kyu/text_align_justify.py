71bf2be82c2911610d97c2eb9b7f6b6241a1dc07
Pablo <pablo.velibarquin@alum.uca.es>
2023-12-23 14:38:24 +0100

    Cambios en el código

--- Python/4Kyu/text_align_justify.py
+++ Python/4Kyu/text_align_justify.py
@@ -4,38 +4,40 @@ from math import ceil
 
 def justify(text, width):
     '''Función que realiza la justificación del texto'''
-    print(text, width)
-    sol = ''
+    solution = ''
     while text and len(text) > width:
-        adj = 0
-        for adj in range(width, -1, -1):
-            if text[adj] == ' ':
+        for break_line in range(width, -1, -1):
+            if text[break_line] == ' ':
                 break
-        if adj == width:
-            sol += text[:adj] + '''\n'''
-            text = text[adj:]
+
+        cut_line = text[:break_line]  # Corto la linea en la máxima cantidad de texto posible
+        text = text[break_line + 1:]
+        if len(cut_line) == width or len(cut_line.split()) == 1:
+            solution += cut_line + '''\n'''  # La añadimos a la solución
             continue
-        old_line = text[:adj+1]
-        text = text[adj+1:]
-        words = old_line.strip().split()
-        start, end = words[0], words[-1]
-        if start == end:
-            sol += start + '''\n'''
+
+        words = cut_line.split()    # Separo todas las palabras
+        white_spaces = width - len(''.join(words))  # Obtengo cuantos espacios libres tengo que repartir
+        gap = ceil(white_spaces / (len(words) - 1))  # Saco el máximo gap entre palabras
+
+        space = gap * ' '
+        if len(space.join(words)) == width:  # Si al añadir el gap a todas las palabras se ajusta al ancho
+            solution += space.join(words) + '''\n'''  # La añadimos a la solución
             continue
-        characters = len(''.join(words))
-        white_spaces = width - characters
-        if white_spaces:
-            fill = ceil(white_spaces / (len(words) - 1))
-        else:
-            fill = 1
-        if len(words) <= 2:
-            sol += start + (fill) * ' ' + end + '''\n'''
-        else:
-            for word in words[1:-1]:
-                start += fill * ' ' + word
-            sol += start + (fill-1) * ' ' + end + '''\n'''
-    sol += text
-    return sol
+
+        new_line = words.pop(0) # Creamos una nueva linea
+        while words:
+            new_line += gap * ' '   # Añadimos espacio original
+            space = (gap-1) * ' '   # Creamos un espacio nuevo con 1 menos de ancho
+            if len(new_line + space.join(words)) == width:
+                new_line += space.join(words)   # Lo unimos todo en una nueva linea
+                break
+            new_line += words.pop(0)    # Si no, añadimos una palabra más manteniendo el espacio original
+
+        solution += new_line + '''\n''' # Añadimos salto de linea
+
+    solution += text.strip()
+    return solution
 
 if __name__ == '__main__':
     cadena = justify('123 45 6', 7)
